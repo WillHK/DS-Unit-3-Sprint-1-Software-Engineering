9fad698831e98bf052b86f6607cab8a223e3f4ab
Will Haeck <will.haeck@gmail.com>
2019-07-09 16:04:09 -0700

    report on if null, blank or question mark values exist, need to add details on which columns and % of values

--- module2-oop-code-style-and-reviews/lambdatawillhk.py
+++ module2-oop-code-style-and-reviews/lambdatawillhk.py
@@ -12,17 +12,33 @@ class LambData:
         column_labels = df.select_dtypes(exclude=["number","bool_","object_"]).columns.tolist()
         return column_labels
     
-    def check_df_for_blanks(self, df: pd.DataFrame):
+    def check_categoricals_for_blanks(self, df: pd.DataFrame):
         df = df.copy()
+        mask = pd.DataFrame()
         column_labels = self.get_categorical_column_labels(df)
         for col in column_labels:
-            df[col] = df[col].apply(lambda x: True if x.strip(' ')=='' else False)
+            mask[col] = df[col].apply(lambda x: 1 if x.strip(' ')=='' else 0)
+        return mask
+    
+    def check_categoricals_for_question_marks(self, df:pd.DataFrame):
+        df = df.copy()
+        mask = pd.DataFrame()
+        column_labels = self.get_categorical_column_labels(df)
+        for col in column_labels:
+            mask[col] = df[col].apply(lambda x: 1 if x.strip(' ')=='?' else 0)
+        return mask
 
     def null_check(self, df):
         df = df.copy()
         nulls = df.isna()
-        blanks = self.check_df_for_blanks(df)
-        pass
+        blanks = self.check_categoricals_for_blanks(df)
+        question_marks = self.check_categoricals_for_question_marks(df)
+        if nulls.sum().sum() > 0:
+            print('This dataframe has null values.')
+        if blanks.sum().sum() > 0:
+            print('This dataframe has categorical columns with empty strings as values.')
+        if question_marks.sum().sum() > 0:
+            print('This dataframe has categorical columns with lone question marks as values.')
     
     def add_column_to_df(self, df: pd.DataFrame, new_col: list, name: str):
         df = df.copy()
